6bf0284504ab579d1b3e336fc36b64291352f587
choonkeun <choonkeun@yahoo.com>
2023-02-25 13:50:08 -0800

    Initial Upload

---
+++ AADIdentity-NETCore 3.1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AADIdentity_NETCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AADIdentity_NETCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //var a = User.Identity.Name;
+            //var b = HttpContext.User.Claims;
+            //var claims = b.ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
